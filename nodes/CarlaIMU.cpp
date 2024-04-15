72ee37ea72b1c17e427cc643511b0127f7df4732
ysrhee6199 <yongse6199@gmail.com>
2024-04-15 22:47:03 +0900

    add_imu_

---
+++ nodes/CarlaIMU.cpp
@@ -0,0 +1,22 @@
+#include "CarlaIMU.hpp"
+#include <boost/make_shared.hpp>
+
+CarlaIMUPublisher::CarlaIMUPublisher(boost::shared_ptr<carla::client::Vehicle> vehicle_,std::string name_)
+    : Node("carla_lidar_publisher", rclcpp::NodeOptions()
+               .allow_undeclared_parameters(true)
+           .automatically_declare_parameters_from_overrides(true)),Vehicle_(vehicle_) {
+
+            IMUPublisher_ = this->create_publisher<std_msgs::msg::Float32MultiArray>(name_ + "accel",1);
+            timer_1ms = this->create_wall_timer(
+            1ms, std::bind(&CarlaIMUPublisher::CarlaIMUPublisher_callback, this));
+           }
+
+void CarlaIMUPublisher::CarlaIMUPublisher_callback() {
+    carla::geom::Vector3D acc_ = Vehicle_->GetAcceleration();
+    auto message = std_msgs::msg::Float32MultiArray();
+    message.data.push_back(acc_.x);
+    message.data.push_back(acc_.y);
+    message.data.push_back(acc_.z);
+    IMUPublisher_->publish(message);
+}
+
